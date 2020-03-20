7baf090d44e4323c11addbe59b4be053bb721da2
sagara <yuya.sagara.0627@gmail.com>
2020-03-20 11:22:34 +0900

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const User = ({ name, skills }) => {
+  // const name = props.name
+  // const skills = props.skills
+  const [score, setScore] = useState(0)
+  const handleClick = () => {
+    setScore(score + 1)
+  }
+  return (
+    <>
+      <h3>{name}</h3>
+      <ul>
+        <li>特技:{skills}</li>
+        <li>好きな食べ物:卵</li>
+        <li>嫌いな食べ物:玉ねぎ</li>
+      </ul>
+      <p>Score:{score}</p>
+      <button
+        type="button"
+        onClick={handleClick}
+      >
+        加点
+      </button>
+    </>
+  )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  const myElement = <h3>カキクケコ</h3>
+  const users = {
+    sgr: {
+      name: "サガラ",
+      skills: "寝る"
+    }
+  }
+  return (
+    <>
+      <h1>Hello,world!</h1>
+      <h2>アイウエオ</h2>
+      {myElement}
+      <User name={users.sgr.name} skills={users.sgr.skills} />
+      <User name={users.sgr.name} skills={users.sgr.skills} />
+    </>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+)
