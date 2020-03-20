841daa28f9d17a9ed63382c505bc5b8aae41bb1c
sagara <yuya.sagara.0627@gmail.com>
2020-03-20 12:33:23 +0900

    授業で書いたコードをもう一度見ずに書いてみる

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from "react"
+import ReactDOM from "react-dom"
 
-const User = ({ name, skills }) => {
-  // const name = props.name
-  // const skills = props.skills
+const User = ({ name, skills, like, dislike }) => {
   const [score, setScore] = useState(0)
   const handleClick = () => {
     setScore(score + 1)
@@ -12,11 +10,11 @@ const User = ({ name, skills }) => {
     <>
       <h3>{name}</h3>
       <ul>
-        <li>特技:{skills}</li>
-        <li>好きな食べ物:卵</li>
-        <li>嫌いな食べ物:玉ねぎ</li>
+        <li>特技{skills}</li>
+        <li>好きな食べ物{like}</li>
+        <li>嫌いな食べ物{dislike}</li>
       </ul>
-      <p>Score:{score}</p>
+      <p>score:{score}</p>
       <button
         type="button"
         onClick={handleClick}
@@ -28,20 +26,20 @@ const User = ({ name, skills }) => {
 }
 
 const App = () => {
-  const myElement = <h3>カキクケコ</h3>
   const users = {
     sgr: {
-      name: "サガラ",
-      skills: "寝る"
+      name: "相良",
+      skills: "寝る",
+      like: "たまご",
+      dislike: "玉ねぎ"
     }
   }
   return (
     <>
-      <h1>Hello,world!</h1>
+      <h1>Hello, World!</h1>
       <h2>アイウエオ</h2>
-      {myElement}
-      <User name={users.sgr.name} skills={users.sgr.skills} />
-      <User name={users.sgr.name} skills={users.sgr.skills} />
+      <User name={users.sgr.name} skills={users.sgr.skills} like={users.sgr.like} dislike={users.sgr.dislike} />
+      <User name={users.sgr.name} skills={users.sgr.skills} like={users.sgr.like} dislike={users.sgr.dislike} />
     </>
   )
 }
